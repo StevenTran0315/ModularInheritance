988ca66b1c2c966f98f9950d26117f0a73a97c20
S331461152 <S331461152@LDHS-BCT-100893.ad.ocdsb.ca>
2019-10-24 09:26:06 -0400

    first commit

---
+++ NetBeansProjects/Modular Inheritance/src/modular/inheritance/ModularInheritance.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modular.inheritance;
+
+/**
+ *
+ * @author S331461152
+ */
+public class ModularInheritance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
