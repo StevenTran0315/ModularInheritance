96f5b71a1d6e9e1a6490b5d432e1f97ec8f1102d
Steven <Steven@192.168.0.10>
2019-10-27 16:40:50 -0400

    asking what shape they want

--- NetBeansProjects/Modular Inheritance/src/modular/inheritance/Shape.java
+++ NetBeansProjects/Modular Inheritance/src/modular/inheritance/Shape.java
@@ -15,6 +15,7 @@ public class Shape {
         static Scanner sc = new Scanner(System.in);
         static int ShapeType;
         static int CalcType;
+        static int PolyType;
         static boolean area;
     
     /**
@@ -22,7 +23,30 @@ public class Shape {
      */
         
     private static void ChoosePolygon(){
+        System.out.print("What shape  do you want to calculate? ");
         
+        System.out.println("0 : Exit");
+        System.out.println("1 : Quadrilateral");
+        System.out.println("2 : Triangle");
+
+        while(0==0){
+        PolyType = sc.nextInt();
+             
+            switch(ShapeType){
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1://Send user to 2d shape directory
+                    //Quad
+                    break;
+                case 2:
+                    //triangle stuff
+                    break;
+                default://All other choices are invalid
+                    System.out.println("Invalid. Try again");
+                    break;
+           }
+        }
     }    
         
     private static void ChooseShape(){
@@ -59,6 +83,7 @@ public class Shape {
             System.out.println("Not a valid Choice.");
          }  
         }
+        
         ChooseShape();
        
         
