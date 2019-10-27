6fb7eab24c040d0b44fe6be4a5226ee11a7d5596
liamu <liamu@192.168.2.30>
2019-10-27 16:27:11 -0400

    did calculations for area and perimeter of triangles and quadrilaterals

---
+++ NetBeansProjects/Modular Inheritance/src/modular/inheritance/Triangle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modular.inheritance;
+
+/**
+ *
+ * @author liamu
+ */
+public class Triangle extends Shape{
+    public double TriangleArea(double Base, double Height){
+        return (Base*Height)/2;
+    }
+    
+    public double TrianglePerimeter(double sideA, double sideB, double sideC){
+        return sideA+sideB+sideC;
+    }
+   
+}
